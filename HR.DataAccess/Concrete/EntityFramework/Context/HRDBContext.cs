f70593ead02723ad3ccdbf1c3725fefd2aa1a316
Ramiz Veliyev <vramiz001@gmail.com>
2022-10-10 17:20:26 +0400

    Test

--- HR.DataAccess/Concrete/EntityFramework/Context/HRDBContext.cs
+++ HR.DataAccess/Concrete/EntityFramework/Context/HRDBContext.cs
@@ -1,35 +1,26 @@
-<<<<<<< HEAD
-﻿using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography.X509Certificates;
-=======
-﻿using HR.DataAccess.Concrete.EntityFramework.Mappings;
+﻿using Core.DataAccess.Concrete.EntityFramework.Mappings;
+using Core.Entities.Concrete;
+using HR.DataAccess.Concrete.EntityFramework.Mappings;
 using HR.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
->>>>>>> origin/arzu-branch-1
 
 namespace HR.DataAccess.Concrete.EntityFramework.Context
 {
     public class HRDBContext : DbContext
     {
-<<<<<<< HEAD
-=======
+        #region DbSet
         public DbSet<CalendarDay> CalendarDays { get; set; }
-        public DbSet<Employee> Employees { get; set; }
 
->>>>>>> origin/arzu-branch-1
+        #endregion
         public HRDBContext(DbContextOptions<HRDBContext> options) : base(options)
         {            
 
-<<<<<<< HEAD
-=======
 
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new CalendarDayMap());
-            modelBuilder.ApplyConfiguration(new EmployeeMap());
->>>>>>> origin/arzu-branch-1
         }
     }
 }

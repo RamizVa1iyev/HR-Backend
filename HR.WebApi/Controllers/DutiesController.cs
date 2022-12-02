d0aa7fe2ecd1d802f5e7a3f1df32ab79979b2924
TemporaryProfile2009 <asim.alizada9@gmail.com>
2022-12-02 16:50:14 +0400

    Fixed

---
+++ HR.WebApi/Controllers/DutiesController.cs
@@ -0,0 +1,18 @@
+﻿using Core.WebAPI;
+using HR.Business.Abstract;
+using HR.Entities.Concrete;
+using HR.Entities.Models.RequestModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DutiesController : BaseController<IDutyService, Duty, DutyAddRequestModel, DutyUpdateRequestModel, DutyDeleteRequestModel>
+    {
+        public DutiesController(IDutyService service) : base(service)
+        {
+        }
+    }
+}

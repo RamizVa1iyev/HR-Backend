6c8d7bcba131ffec7803b0cb5f0df18d5f06a6ed
arzuteymurova <arzuteymurova95@gmail.com>
2022-12-08 11:31:00 +0400

    Fixed

---
+++ HR.WebApi/Controllers/EmployeeRewardsController.cs
@@ -0,0 +1,18 @@
+﻿using Core.WebAPI;
+using HR.Business.Abstract;
+using HR.Entities.Concrete;
+using HR.Entities.Models.RequestModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeRewardsController : BaseController<IEmployeeRewardService, EmployeeReward, EmployeeRewardAddRequestModel, EmployeeRewardUpdateRequestModel, EmployeeRewardDeleteRequestModel>
+    {
+        public EmployeeRewardsController(IEmployeeRewardService service) : base(service)
+        {
+        }
+    }
+}

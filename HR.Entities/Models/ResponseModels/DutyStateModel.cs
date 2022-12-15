e0bcadfbec5b702f21de59c4d6c0ac873d13f34d
TemporaryProfile2009 <asim.alizada9@gmail.com>
2022-12-15 14:37:15 +0400

    Fixed.

---
+++ HR.Entities/Models/ResponseModels/DutyStateModel.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Abstract;
+using HR.Entities.Concrete;
+
+namespace HR.Entities.Models.ResponseModels
+{
+    public class DutyStateModel : IModel
+    {
+        public List<Duty> Duties { get; set; }
+        public List<State> States { get; set; }
+    }
+}
--- HR.WebApi/Controllers/DutiesController.cs
+++ HR.WebApi/Controllers/DutiesController.cs
@@ -2,6 +2,7 @@
 using HR.Business.Abstract;
 using HR.Entities.Concrete;
 using HR.Entities.Models.RequestModels;
+using HR.Entities.Models.ResponseModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +12,22 @@ namespace HR.WebApi.Controllers
     [ApiController]
     public class DutiesController : BaseController<IDutyService, Duty, DutyAddRequestModel, DutyUpdateRequestModel, DutyDeleteRequestModel>
     {
-        public DutiesController(IDutyService service) : base(service)
+        private readonly IStateService _stateService;
+        public DutiesController(IDutyService service, IStateService stateService) : base(service)
         {
+            _stateService = stateService;
+        }
+
+        [HttpGet("getAllStatesDuties")]
+        public IActionResult GetAllStatesDuties()
+        {
+            var model = new DutyStateModel
+            {
+                Duties = Service.GetAll(),
+                States = _stateService.GetAll()
+            };
+
+            return Ok(model);
         }
     }
 }
